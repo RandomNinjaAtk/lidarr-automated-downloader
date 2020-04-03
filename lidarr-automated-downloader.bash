0c98d6d126c27f9fcd6073b47796f2777e315041
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-04-03 13:01:53 -0400

    show beets matching

--- lidarr-automated-downloader.bash
+++ lidarr-automated-downloader.bash
@@ -160,7 +160,7 @@ beetstagging () {
 	sleep 0.1
 	
 	if find "$downloaddir" -type f -iregex ".*/.*\.\(flac\|opus\|m4a\|mp3\)" | read; then
-		beet -c "${BeetConfig}" -l "${BeetLibrary}" -d "$downloaddir" import -q "$downloaddir" > /dev/null
+		beet -c "${BeetConfig}" -l "${BeetLibrary}" -d "$downloaddir" import -q "$downloaddir"
 		if find "$downloaddir" -type f -iregex ".*/.*\.\(flac\|opus\|m4a\|mp3\)" -newer "$downloaddir/beets-match" | read; then
 			echo "SUCCESS: Matched with beets!"
 		else
